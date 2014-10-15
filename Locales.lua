1847e8097dcf3f215c02df60b48ed44ea2076e62
Patrick Schriner <p.schriner@gmail.com>
2014-10-16 01:35:37 +0200

    Localization

--- Locales.lua
+++ Locales.lua
@@ -3,12 +3,12 @@ local debug = false
 debug = true
 --@end-debug@
 
-local L = LibStub("AceLocale-3.0"):NewLocale("LDB-Vengeance", "enUS", true, debug)
+local L = LibStub("AceLocale-3.0"):NewLocale("LDB-Resolve", "enUS", true, debug)
 if L then
 --@localization(locale="enUS", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
 end
 
-L = LibStub("AceLocale-3.0"):NewLocale("LDB-Vengeance", "deDE")
+L = LibStub("AceLocale-3.0"):NewLocale("LDB-Resolve", "deDE")
 if L then
 --@localization(locale="deDE", format="lua_additive_table", same-key-is-true=true, handle-subnamespaces="concat")@
 end
